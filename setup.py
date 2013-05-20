6f239621004418a61e783a7fffc0c1bb56a9259d
Brandon Hamric <brandonhamric@gmail.com>
2013-05-20 13:42:33 -0500

    modified:   setup.py added distribute's 2to3 option for python 3 compatibility

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
                 "Topic :: Software Development :: Libraries :: Python Modules",
 
         ],
+        use_2to3 = True,
         long_description = """\
 Iron.io common library
 ----------------------
