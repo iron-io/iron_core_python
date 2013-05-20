933d546ab7f73f4c1143fa4a13597d55d0867ce8
Brandon Hamric <brandonhamric@gmail.com>
2013-05-20 15:30:40 -0500

    python3 doesn't like https://domain:443... stupid python3. added a conditional to set self.base_url without the port number if https is specified as the protocol

--- iron_core.py
+++ iron_core.py
@@ -94,7 +94,10 @@ class IronClient:
         }
         if self.token:
             self.headers["Authorization"] = "OAuth %s" % self.token
-        self.base_url = "%s://%s:%s/%s/" % (self.protocol, self.host,
+        if self.protocol =="https" and self.port == 443:
+            self.base_url = "%s://%s/%s/" % (self.protocol, self.host, self.api_version)
+        else:
+            self.base_url = "%s://%s:%s/%s/" % (self.protocol, self.host,
                 self.port, self.api_version)
         if self.project_id:
             self.base_url += "projects/%s/" % self.project_id
