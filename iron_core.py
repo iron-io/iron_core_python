8dbc60a6340debcc390796f455f9fe70ebc4ef67
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2014-09-02 14:26:40 +0600

    change module version and corrected request header

--- iron_core.py
+++ iron_core.py
@@ -26,11 +26,16 @@ class KeystoneTokenProvider:
         self.username = keystone["username"]
         self.password = keystone["password"]
         self.token = None
-        self.local_expires_at = datetime_mod.timedelta(seconds=0)
+        self.local_expires_at = None
 
 
     def getToken(self):
-        if self.token is None or (datetime.now() - self.local_expires_at) > -10:
+        if self.local_expires_at:
+            date_diff = time.mktime(datetime.now().timetuple()) - time.mktime(self.local_expires_at.timetuple())
+        else:
+            date_diff = time.mktime(datetime.now().timetuple())
+
+        if self.token is None or date_diff > -10:
             payload = {
                 'auth': {
                     'tenantName': self.tenant,
@@ -58,7 +63,7 @@ class KeystoneTokenProvider:
 
 
 class IronClient:
-    __version__ = "1.1.1"
+    __version__ = "1.1.2"
 
     def __init__(self, name, version, product, host=None, project_id=None,
                  token=None, protocol=None, port=None, api_version=None,
@@ -170,7 +175,7 @@ class IronClient:
 
     def _doRequest(self, url, method, body="", headers={}):
         if self.token or self.keystone:
-            self.headers["Authorization"] = "OAuth %s" % self.token_provider.getToken()
+            headers["Authorization"] = "OAuth %s" % self.token_provider.getToken()
         if method == "GET":
             r = self.conn.get(url, headers=headers)
         elif method == "POST":
