654350539374951bc852c57e0abe92a72cef24fd
Max Countryman <maxc@me.com>
2015-03-18 08:15:07 -0700

    move conn to request method
    This moves the connection session into the request method in order to
    facilitate better support for multiprocessing. In particular, a Requests
    Session object used across processes will fail to properly encrypt data
    via SSL[0][1]. By encapsulating the session creation in the request
    method itself we workaround this issue at the limited expense of
    creating the session on demand.
    
    A version bump, from 1.1.7 to 1.1.8, is included as well.
    
    Fixes errors that look like: [Errno 1] _ssl.c:1429
    
    [0] http://stackoverflow.com/a/3724938/1953283
    [1] https://github.com/kennethreitz/requests/issues/1906

--- iron_core.py
+++ iron_core.py
@@ -61,7 +61,7 @@ class KeystoneTokenProvider(object):
 
 
 class IronClient(object):
-    __version__ = "1.1.2"
+    __version__ = "1.1.8"
 
     def __init__(self, name, version, product, host=None, project_id=None,
                  token=None, protocol=None, port=None, api_version=None,
@@ -158,7 +158,6 @@ class IronClient(object):
         self.port = config["port"]
         self.api_version = config["api_version"]
         self.cloud = config["cloud"]
-        self.conn = requests.Session()
         self.headers = {
                 "Accept": "application/json",
                 "User-Agent": "%s (version: %s)" % (self.name, self.version)
@@ -185,16 +184,18 @@ class IronClient(object):
         if self.token or self.keystone:
             headers["Authorization"] = "OAuth %s" % self.token_provider.getToken()
 
+        conn = requests.Session()
+
         if method == "GET":
-            r = self.conn.get(url, headers=headers)
+            r = conn.get(url, headers=headers)
         elif method == "POST":
-            r = self.conn.post(url, data=body, headers=headers)
+            r = conn.post(url, data=body, headers=headers)
         elif method == "PUT":
-            r = self.conn.put(url, data=body, headers=headers)
+            r = conn.put(url, data=body, headers=headers)
         elif method == "DELETE":
-            r = self.conn.delete(url, data=body, headers=headers)
+            r = conn.delete(url, data=body, headers=headers)
         elif method == "PATCH":
-            r = self.conn.patch(url, data=body, headers=headers)
+            r = conn.patch(url, data=body, headers=headers)
         else:
             raise ValueError("Invalid HTTP method")
         return r
